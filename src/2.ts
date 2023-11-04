4da060d47264b553ba8a3166e8c33e0154d3cfbb
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-11-04 18:06:06 +0200

    update

--- src/2.ts
+++ src/2.ts
@@ -35,9 +35,29 @@
 //   // Реалізуйте конструктор та збільшіть salary на 10000
 // }
 
-
 // export {};
 
 //====================================
 //*====== Вирішення завдання 2 =======
 //====================================
+class Employee {
+  public name: string;
+  protected department: string;
+  protected salary: number;
+
+  constructor(name: string, department: string, salary: number) {
+    this.name = name;
+    this.department = department;
+    this.salary = salary;
+  }
+
+  getEmployeeDetails() {
+    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+  }
+}
+
+class Manager extends Employee {
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
+}
